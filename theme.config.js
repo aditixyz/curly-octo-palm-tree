5933cd3371cb97af0dd66eb8d7869161266f25a2
aditi <40533629+aditixyz@users.noreply.github.com>
2023-02-15 12:15:05 +0530

    Add files via upload

---
+++ theme.config.js
@@ -0,0 +1,21 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Aditi De.
+      <a href="https://twitter.com/aditide_">Twitter</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  )
+}
